e3364fc2cc9298bd611df01a393ce94e388c2731
Elizabeth <itsybitsy1121@outlook.com>
2024-04-09 21:13:17 -0800

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,168 @@
+﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            // create a list of Insurees
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+
+                //create a list of insurees 
+                var insurees = db.Insurees;
+
+                var insureeVms = new List<InsureeVm>();
+                foreach (var insuree in insurees)
+                {
+                    var insureeVm = new InsureeVm();
+                    insureeVm.Id = insuree.Id;
+                    insureeVm.FirstName = insuree.FirstName;
+                    insureeVm.LastName = insuree.LastName;
+                    insureeVm.EmailAddress = insuree.EmailAddress;
+                    insureeVm.DateOfBirth = insuree.DateOfBirth;
+                    insureeVm.CarYear = insuree.CarYear;
+                    insureeVm.CarMake = insuree.CarMake;
+                    insureeVm.CarModel = insuree.CarModel;
+                    insureeVm.DUI = insuree.DUI;
+                    insureeVm.SpeedingTickets = insuree.SpeedingTickets;
+                    insureeVm.CoverageType = insuree.CoverageType;
+                    insureeVm.Quote = insuree.Quote;
+                    insureeVms.Add(insureeVm);
+                }
+                return View(insureeVms);
+            }
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        //show all quotes and the insuree's name issued in the insurees list
+        public ActionResult AllQuotesIssued()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insurees = db.Insurees;
+                var insureeVms = new List<InsureeVm>();
+                foreach (var insuree in insurees)
+                {
+                    if(insuree.Quote > 0)
+                    {
+                        var insureeVm = new InsureeVm();
+                        //insureeVm.Id = insuree.Id;
+                        insureeVm.FirstName = insuree.FirstName;
+                        insureeVm.LastName = insuree.LastName;
+                        insureeVm.EmailAddress = insuree.EmailAddress;
+                        insureeVm.Quote = insuree.Quote;
+                        insureeVms.Add(insureeVm);
+                    }
+                }
+                return View(insureeVms);
+            }
+        }
+    }
+
+}
